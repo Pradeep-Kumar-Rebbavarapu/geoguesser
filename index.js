c68274d5e3019541e1e7e2e9686475900fb069f1
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-08-06 16:54:45 +0530

    Added Timer and schedule Jobs

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require('express-session');
+const { Timer } = require('timer-node');
 const cors = require("cors");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const passport = require('passport');
+const schedule = require('node-schedule');
+let isJobRunning = false;
 const app = express();
 const Image = require("./models/Image");
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
@@ -13,6 +16,10 @@ app.use(passport.session());
 function isAuthenticated(req, res, next) {
   req.user ? next() : res.redirect('/LoginPage');
 }
+const timer = new Timer({
+  label: 'test-timer',
+  startTimestamp: Date.now() // 2019-07-14 03:13:21.233Z
+});
 const port = process.env.PORT || 3000;
 
 const corsConfig = {
@@ -29,14 +36,15 @@ app.use(express.static(__dirname + "/public"));
 
 
 app.get('/auth/google',
-  passport.authenticate('google', { scope: [ 'email', 'profile' ] }
-));
+  passport.authenticate('google', { scope: ['email', 'profile'] }
+  ));
 
-app.get( '/auth/google/callback',
-  passport.authenticate( 'google', {
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
     successRedirect: '/dashboard',
     failureRedirect: '/'
   })
+
 );
 
 
@@ -44,13 +52,21 @@ app.get( '/auth/google/callback',
 
 const dbURI =
   "mongodb+srv://Ayush:Ayush2003@cluster0.3gnxxme.mongodb.net/geoguesser?retryWrites=true&w=majority";
-
+var server;
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB!");
-    app.listen(port, () => {
+    server = app.listen(port, () => {
+      timer.start();
       console.log(`Server is running on http://localhost:${port}`);
+      const scheduled_date = new Date(2023, 7, 9,23,59,0);
+      console.log(scheduled_date)
+      const job = schedule.scheduleJob(scheduled_date, function () {
+        isJobRunning = true;
+        timer.stop()
+        schedule.cancelJob(job);
+      });
     });
   })
   .catch((error) => {
@@ -65,91 +81,131 @@ const authRoutes = require("./routes/auth");
 
 app.use(authRoutes);
 
+
+
 app.get("/", (req, res) => {
-  res.render("login",{
-    user: req.user
+ 
+  res.render("login", {
+    user: req.user,
+    time:timer.time()
   });
 });
 
-app.get('/aboutus',(req,res)=>{
-  res.render('aboutus',{
-    user: req.user
+app.get('/aboutus', (req, res) => {
+  res.render('aboutus', {
+    user: req.user,
+    time:timer.time()
   })
 })
 
-app.get('/LoginPage',(req,res)=>{
-  res.render('LoginPage',{
-    user: req.user
-  })
+app.get('/LoginPage', (req, res) => {
+  if (!isJobRunning) {
+    res.render('LoginPage', {
+      user: req.user,
+      time:timer.time()
+    })
+  }
+  else {
+    res.redirect('/leaderboard')
+  }
 }
 )
 
 
 app.get("/dashboard", isAuthenticated, async (req, res) => {
-  
-  const { email } = req.user;
-  const leaderboard = await User.find({ score: { $exists: true } }).sort({
-    score: -1,
-  });
-  const user = await User.findOne({ email });
-  const image = await Image.findOne({ _id: user.level });
- 
-  //get the length of data in the table Image
-  const length = await Image.countDocuments();
-
-
-  if(user.level===length+1){
-    res.render("leaderboard", { leaderboard,user:req.user });
-  }
-  else{
-    var img_url = image.path;
-    var lat = image.long;
-    var long = image.lat;
-    res.render("dashboard", {
-      user: { email },
-      img_url: img_url,
-      lat: lat,
-      long: long,
+  if (!isJobRunning) {
+    const { email } = req.user;
+    const leaderboard = await User.find({ score: { $exists: true } }).sort({
+      score: -1,
     });
+    const user = await User.findOne({ email });
+    const image = await Image.findOne({ _id: user.level });
 
+    //get the length of data in the table Image
+    const length = await Image.countDocuments();
+
+
+    if (user.level === length + 1) {
+      res.render("leaderboard", { leaderboard, user: req.user });
+    }
+    else {
+      var img_url = image.path;
+      var lat = image.long;
+      var long = image.lat;
+      res.render("dashboard", {
+        user: { email },
+        img_url: img_url,
+        lat: lat,
+        long: long,
+        time:timer.time()
+      });
+
+    }
+  }
+  else {
+    res.redirect('/leaderboard')
   }
 });
 
 app.get("/leaderboard", isAuthenticated, async (req, res) => {
+
   try {
     const leaderboard = await User.find({ score: { $exists: true } }).sort({
       score: -1,
     });
-   
-    res.render("leaderboard", { leaderboard,user:req.user });
+
+    res.render("leaderboard", { leaderboard, user: req.user,time:timer.time() });
   } catch (error) {
     console.error("Error fetching leaderboard:", error);
-    res.redirect("dashboard",{user:req.user});
+    res.redirect("dashboard", { user: req.user,time:timer.time() });
   }
-});
+}
+);
 
 app.get("/register", (req, res) => {
   res.render("register");
 });
 
 app.post("/update-score", isAuthenticated, async (req, res) => {
-  try {
-    let { score } = req.body;
-    const { email } = req.user;
-    const user = await User.findOne({ email });
-    user.score += Math.round(score);
-    user.level += 1;
-    await user.save();
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
+  if (!isJobRunning) {
+    try {
+      let { score } = req.body;
+      const { email } = req.user;
+      const user = await User.findOne({ email });
+      user.score += Math.round(score);
+      user.level += 1;
+      await user.save();
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      res.redirect('/dashboard')
+    } catch (error) {
+
     }
-    res.redirect('/dashboard')
-  } catch (error) {
-    
   }
+  else {
+    res.redirect('/leaderboard')
+  }
+
 });
 
+
+
+
+
+
 app.use("/auth/logout", (req, res) => {
-  req.session.destroy();
-  res.redirect("/");
+  if (!isJobRunning) {
+    req.session.destroy();
+    res.redirect("/");
+  }
+  else {
+    res.redirect('/leaderboard')
+  }
+
+});
+
+
+app.use((req, res, next) => {
+  console.log(req.path)
 });
