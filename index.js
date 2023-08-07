6f2b5dc5f6f1a04770531aae8116af6d6660300a
PRADEEP KUMAR REBBAVARAPU <103821866+Pradeep-Kumar-Rebbavarapu@users.noreply.github.com>
2023-08-07 09:58:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -124,7 +124,8 @@ app.get("/dashboard", isAuthenticated, async (req, res) => {
     //get the length of data in the table Image
     const length = await Image.countDocuments();
 
-
+    console.log("length",length)
+    console.log("user level",user.level)
     if (user.level === length + 1) {
       res.render("leaderboard", { leaderboard, user: req.user });
     }
